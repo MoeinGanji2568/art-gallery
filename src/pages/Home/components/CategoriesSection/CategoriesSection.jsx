7920420e9e2566aba3c5f7974087f16378a7826a
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-05 17:30:57 +0330

    feat: add more button for category section because in the mobile view, the number of items increased and the user had to scroll a lot.

--- src/pages/Home/components/CategoriesSection/CategoriesSection.jsx
+++ src/pages/Home/components/CategoriesSection/CategoriesSection.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useData } from "../../../../contexts/DataProvider.js";
 import { Link } from "react-router-dom";
 import "./CategoriesSection.css";
+import Button from "../../../../components/Ui/Button.jsx";
 
 export const CategoriesSection = () => {
+  const [showAll, setShowAll] = useState(false);
   const { state, dispatch } = useData();
   return (
     <div>
       <h1 className="categories-heading">Shop By Categories</h1>
-      <div className="categories-container">
+      <div
+        className="categories-container"
+        style={{ maxHeight: `${showAll ? "fit-content" : "600px"}` }}
+      >
         {state.allCategories.map(({ _id, categoryName, img }) => (
           <Link
             onClick={() =>
@@ -28,6 +33,10 @@ export const CategoriesSection = () => {
           </Link>
         ))}
       </div>
+      <div className="fade"></div>
+      <Button onclick={() => setShowAll(!showAll)}>
+        {showAll ? "Show less ..." : "Show more ..."}
+      </Button>
     </div>
   );
 };
