5c17db382d002bfbd39f85777e02f2e0f40c6aab
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-06 10:47:44 +0330

    fix: delete description

--- src/pages/ProductListing/components/ProductListingSection/ProductSkeleton.jsx
+++ src/pages/ProductListing/components/ProductListingSection/ProductSkeleton.jsx
@@ -6,33 +6,33 @@ const ProductSkeleton = () => {
   return (
     <div className="product-card">
       <div className="product-card-image">
-        <Skeleton height={200} /> {/* جایگزین تصویر */}
+        <Skeleton height={200} />
       </div>
       <div className="product-card-details">
         <h3>
-          <Skeleton width={150} /> {/* جایگزین نام محصول */}
+          <Skeleton width={150} />
         </h3>
         <p className="ratings">
-          <Skeleton width={100} /> {/* جایگزین ریتینگ */}
+          <Skeleton width={100} />
         </p>
         <div className="price-container">
           <p className="original-price">
-            <Skeleton width={50} /> {/* جایگزین قیمت اصلی */}
+            <Skeleton width={50} />
           </p>
           <p className="discount-price">
-            <Skeleton width={50} /> {/* جایگزین قیمت تخفیف‌خورده */}
+            <Skeleton width={50} />
           </p>
         </div>
         <p>
-          <Skeleton width={100} /> {/* جایگزین دسته‌بندی */}
+          <Skeleton width={100} />
         </p>
         <div className="info">
-          <Skeleton width={80} /> {/* جایگزین وضعیت موجودی */}
-          <Skeleton width={80} /> {/* جایگزین وضعیت ترند بودن */}
+          <Skeleton width={80} />
+          <Skeleton width={80} />
         </div>
       </div>
       <div className="product-card-buttons">
-        <Skeleton width={100} height={40} /> {/* جایگزین دکمه‌ها */}
+        <Skeleton width={100} height={40} />
         <Skeleton width={40} height={40} />
       </div>
     </div>
